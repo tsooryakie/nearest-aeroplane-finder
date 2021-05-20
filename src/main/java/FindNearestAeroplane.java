afcf844be2abb52c889358d06b45de2aa6671b5f
tsooryakie <38289977+tsooryakie@users.noreply.github.com>
2021-05-20 18:54:41 +0100

    added API calls

--- src/main/java/FindNearestAeroplane.java
+++ src/main/java/FindNearestAeroplane.java
@@ -1,14 +1,59 @@
 import java.lang.Math;
 import java.util.HashMap;
+import java.lang.reflect.Type;
+import java.util.Map;
 import kong.unirest.*;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
 public class FindNearestAeroplane {
 
     final static double EARTH_RADIUS = 6371.0;
+    final static String OPENSKY_API_URL = "https://opensky-network.org/api/states/all/";
+
+    public static HashMap<String, Object> parseJsonAPIResponse(String response) {
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<HashMap<String, Object>>() {
+        }.getType();
+        HashMap<String, Object> jsonResponseMap = gson.fromJson(response, type);
+
+        return jsonResponseMap;
+
+    }
+
+    /**
+     * This method calls the OpenSky API and uses the JSON response from the API call to construct
+     * a key-value HashMap of information about the aeroplanes near the input coordinates.
+     *
+     * @param inputCoordinates - LatLong Input Coordinates to find aeroplanes near.
+     * @return HashMap of aeroplane information
+     */
+    public static HashMap<String, Object> callOpenSkyAPI(LatLong inputCoordinates) {
+
+        HashMap<String, Object> responseHashMap = new HashMap<String, Object>();
+        try {
+            HttpResponse<JsonNode> jsonResponse = Unirest.get(OPENSKY_API_URL)
+                    .queryString("lamin", inputCoordinates.getLatitude())
+                    .queryString("lomin", inputCoordinates.getLongitude())
+                    .queryString("lamax", inputCoordinates.getLatitude())
+                    .queryString("lomax", inputCoordinates.getLongitude())
+                    .asJson();
+
+            responseHashMap = parseJsonAPIResponse(jsonResponse.getBody().toString());
+            return responseHashMap;
+
+        } catch (UnirestException exception) {
+            System.out.println("OpenSky API took too long to respond. Exiting...");
+        }
+
+        return responseHashMap;
+    }
 
     /**
      * This method calculates the Great Circle distance between a given set of coordinates,
      * and a plane currently flying closest to the given set of coordinates.
+     *
      * @param inputCoordinates - HashMap of Latitude and Longitude coordinates of the input location.
      * @param planeCoordinates - HashMap of Latitude and Longitude coordinates of the plane location.
      * @return greatCircleDistance - Spherical distance of plane from given coordinates.
@@ -26,15 +71,16 @@ public class FindNearestAeroplane {
 
     public static void main(String[] args) {
         LatLong london = new LatLong(51.5074, 0.1278);
-        LatLong cardiff = new LatLong(51.4816, 3.1791);
-
         HashMap<String, Double> londonRadians = london.coordinateToRadians();
-        HashMap<String, Double> cardiffRadians = cardiff.coordinateToRadians();
-        System.out.println(londonRadians);
-        System.out.println(cardiffRadians);
 
-        double x = calculateGreatCircleDistance(londonRadians, cardiffRadians);
-        System.out.println(x);
+        HashMap<String, Object> y = callOpenSkyAPI(london);
+        for(Map.Entry<String, Object> entry: y.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            System.out.println("Key: " + key);
+            System.out.println("\n");
+        }
+
     }
 
 }
